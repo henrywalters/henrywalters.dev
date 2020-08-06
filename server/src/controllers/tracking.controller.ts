e943c7d96b32668fd89c1dca2a0a07a9896a3c00
Henry Walters <hwalters@fusetg.com>
2020-08-06 12:09:00 -0400

    Added proxy header for ip

--- server/src/controllers/tracking.controller.ts
+++ server/src/controllers/tracking.controller.ts
@@ -1,11 +1,11 @@
-import {Controller, Ip, Param, Post} from "@nestjs/common";
+import {Controller, Header, Headers, Ip, Param, Post} from "@nestjs/common";
 import {ApiResponse, ResponseDto} from "../dtos/response.dto";
 import {PageVisit} from "../entities/pageVisit.entity";
 
 @Controller("v1/tracking")
 export class TrackingController {
     @Post("page-visit/:page")
-    public async track(@Ip() ip: string, @Param("page") page: string): Promise<ApiResponse<void, string>> {
+    public async track(@Headers("x-forwarded-for") ip: string, @Param("page") page: string): Promise<ApiResponse<void, string>> {
         try {
             const pageVisit = new PageVisit();
             pageVisit.ip = ip;
